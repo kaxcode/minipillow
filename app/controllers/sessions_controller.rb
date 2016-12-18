6005f575a710ec5b0251b2e88cbb3395337240c4
Kenia Valladarez <keniavalladarez@gmail.com>
2016-12-18 11:17:23 -0500

    Updated application controller, create sesssions controller, and created a new.html.haml for sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+  # logging in
+  def new
+  end
+
+  # handle the post from the login page
+  def create
+    self.current_user = User.from_omniauth(request.env['omniauth.auth'])
+
+    if current_user
+      redirect_to root_path
+    else
+      redirect_to login_path
+    end
+  end
+
+  # logout
+  def destroy
+    sessions[:user_id] = nil
+    redirect_to root_path
+  end
+
+  # Show the failure page
+  def failure
+    # TODO, create failure.html.erb
+  end
+end
